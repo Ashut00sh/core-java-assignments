3b0d230aa09df95144a8e7485f295df171d3d1ce
Ashut00sh <jadhavashutosh42@gmail.com>
2020-09-21 12:53:32 +0530

    stringinput

---
+++ javafile/array29.java
@@ -0,0 +1,3 @@
+// Write a program to print the total number of one-D arrays in a two-D array
+// and
+// the number of elements in every one-D array present in the two-D arrays.
--- javafile/string.java
+++ javafile/string.java
@@ -1,8 +1,20 @@
-Initialize one String type of array and print the elements using for each loop.
+//Initialize one String type of array and print the elements using for each loop.
 
 import java.util.Scanner;
-class String{
-public static void main(String[]args){
 
-}
+class String28 {
+    public static void main(String[] args) {
+        int a;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        String name[] = new String[a];
+        for (int i = 0; i < name.length; i++) {
+            name[i] = s.nextLine();
+
+        }
+        for (String abc : name) {
+            System.out.println(abc);
+        }
+        System.out.println(" ");
+    }
 }
